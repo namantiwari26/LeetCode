c077e4cc8c015e2d35f57a4645677e4bad1cb40f
NAMAN TIWARI <namantiwari2603@gmail.com>
2026-07-31 22:12:02 +0530

    Time: 17 ms (67.35%), Space: 108.1 MB (34.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int num : nums){
             if(set.contains(num)){
                 return true;
@@ -8,8 +8,5 @@ class Solution {
             set.add(num);
         }
         return false;
-            
-        
-
     }
 }
